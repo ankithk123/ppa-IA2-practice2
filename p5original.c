8996cd24e3aab59a7cc231c6aedb0b96d5e0299f
Ankith  K <96900989+ankithk123@users.noreply.github.com>
2022-03-21 06:47:49 +0000

    p5oroiginal

--- p5original.c
+++ p5original.c
@@ -3,6 +3,36 @@
 int input()
 {
   int a;
-  printf("enter the number ")
-    scanf("")
+  printf("enter the number ");
+    scanf("%d",&a);
+  return a;
+}
+int _gcd(int a,int b)
+{
+  int gcd=1;
+  for(int i=2;i<=a && i<=b;i++)
+    {
+      if(a%i==0 && b%i==0)
+      {
+        gcd=i;
+      }
+    }
+  return gcd;
+}
+void out_put(int a,int b,int gcd)
+{
+  printf("GCD of %d and %d is %d \n",a,b,gcd);
+  
+}
+int main()
+{
+  int a,b,gcd;
+  a=input();
+  b=input();
+  gcd=_gcd(a,b);
+ out_put(a,b,gcd);
+  return 0;
+  
+
+  
 }
